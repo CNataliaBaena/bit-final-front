5ce6a468a9c1966636821546d23bb23e468cc186
Nago1531 <baenan301@gmail.com>
2026-04-06 21:42:19 -0500

    Proyecto final

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from './components/shared/header/header';
+import { Navigation } from './components/shared/navigation/navigation';
+import { Footer } from './components/shared/footer/footer';
+import { PageNotFound } from './components/pages/page-not-found/page-not-found';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header, Navigation, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
--- src/app/components/pages/products/products.ts
+++ src/app/components/pages/products/products.ts
@@ -1,9 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { ProductsService } from '../../../services/products';
+import { CurrencyPipe } from '@angular/common';
 
 @Component({
   selector: 'app-products',
-  imports: [],
+  imports: [CurrencyPipe],
   templateUrl: './products.html',
   styleUrl: './products.css',
 })
-export class Products {}
+export class Products implements OnInit {
+
+  products: any[] = [];
+
+  private productsService = inject(ProductsService);
+  ngOnInit(): void {
+    this.productsService.getAllProducts().subscribe((res: any)=>{
+      this.products = res.data;
+      console.log("Productos:", this.products);
+  });
+}
+}
